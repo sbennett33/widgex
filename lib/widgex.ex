034a0520dd84f54fdf73408fd902d34c71d2e4d5
Scott Bennett <scott.e.bennett@gmail.com>
2024-09-08 11:09:58 -0700

    initial commit

---
+++ lib/widgex.ex
@@ -0,0 +1,18 @@
+defmodule Widgex do
+  @moduledoc """
+  Documentation for `Widgex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Widgex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/widgex_test.exs
@@ -0,0 +1,8 @@
+defmodule WidgexTest do
+  use ExUnit.Case
+  doctest Widgex
+
+  test "greets the world" do
+    assert Widgex.hello() == :world
+  end
+end
