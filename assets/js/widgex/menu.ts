f74923e78415fb9d6474affebebdbff4acfa81f1
Scott Bennett <scott.e.bennett@gmail.com>
2025-01-07 21:01:43 -0800

    menu context trigger part

--- assets/js/widgex/menu.ts
+++ assets/js/widgex/menu.ts
@@ -14,7 +14,7 @@ class Menu extends Component<menu.Context, menu.Api> {
   }
 
   render() {
-    const parts = ["trigger", "positioner", "content"];
+    const parts = ["trigger", "context-trigger", "positioner", "content"];
     for (const part of parts) renderPart(this.el, part, this.api);
     this.renderItemGroupLabels();
     this.renderItemGroups();
